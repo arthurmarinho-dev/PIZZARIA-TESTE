a7094e1d401b995d99ceab1e0e3cd6c5e582477c
Arthur Ribeiro Marinho <ribeiromarinhoarthur@gmail.com>
2026-06-27 20:35:10 -0300

    735 - cadastro + imagens + categoria do produto

--- backend/src/controllers/product/CreateProductController.ts
+++ backend/src/controllers/product/CreateProductController.ts
@@ -5,19 +5,23 @@ class CreateProductController {
     async handle(req: Request, res: Response) {
         const { name, price, description, category_id } = req.body
 
-        let banner = ''
-
         const createProductService = new CreateProductService()
 
-        const product = await createProductService.execute({
-            name,
-            price,
-            description,
-            banner,
-            category_id
-        })
+        if (!req.file) {
+            throw new Error("error upload file")
+        } else {
+            const { originalname, filename: banner } = req.file
 
-        res.json(product)
+            const product = await createProductService.execute({
+                name,
+                price,
+                description,
+                banner,
+                category_id
+            })
+
+            res.json(product)
+        }
     }
 }
 
