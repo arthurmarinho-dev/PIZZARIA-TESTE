237dc53150dbe856aef6ad7db122aa73f2070810
Arthur Ribeiro Marinho <ribeiromarinhoarthur@gmail.com>
2026-06-28 15:26:10 -0300

    1035 - finalizando o pedido

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -20,6 +20,7 @@ import { SendOrderController } from './controllers/order/SendOrderController'
 
 import { ListOrdersController } from './controllers/order/ListOrdersController'
 import { DetailOrderController } from './controllers/order/DetailOrderController'
+import { FinishOrderController } from './controllers/order/FinishOrderController'
 
 import { isAuthenticated } from './middlewares/isAuthenticated'
 
@@ -57,5 +58,7 @@ router.put('/order/send', isAuthenticated, new SendOrderController().handle)
 
 router.get('/orders', isAuthenticated, new ListOrdersController().handle)
 router.get('/order/detail', isAuthenticated, new DetailOrderController().handle)
+
+router.put('/order/finish', isAuthenticated, new FinishOrderController().handle)
 
 export { router }
