c94c13df2925d2faf67b37fab089446d68814773
Arthur Ribeiro Marinho <ribeiromarinhoarthur@gmail.com>
2026-07-01 20:18:35 -0300

    91 - criando página de login

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,45 @@
 import styles from './page.module.scss'
+import logoImg from '/public/logo.svg'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Page() {
   return(
-    <main>
-      <h1 className={styles.teste}>Página Login</h1>
-    </main>
+    <>
+      <div className={styles.containerCenter}>
+        <Image
+          src={logoImg}
+          alt='Logo da pizzaria'
+        />
+
+        <section className={styles.login}>
+          <form>
+            <input
+              type='email'
+              required
+              name='email'
+              placeholder='Digite seu email...'
+              className={styles.input}
+            />
+
+            <input
+              type='password'
+              required
+              name='password'
+              placeholder='*******************'
+              className={styles.input}
+            />
+
+            <button type='submit'>
+              Acessar
+            </button>
+          </form>
+
+          <Link href='/signup' className='styles.text'>
+            Não possui uma conta? Cadastre-se
+          </Link>
+        </section>
+      </div>
+    </>
   )
 }
