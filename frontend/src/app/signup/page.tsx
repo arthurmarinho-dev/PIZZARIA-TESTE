be798456a55f28323276fa47dc494665bda4ba46
Arthur Ribeiro Marinho <ribeiromarinhoarthur@gmail.com>
2026-07-01 21:27:22 -0300

    170 - conectando a página de cadastro com a api

--- frontend/src/app/signup/page.tsx
+++ frontend/src/app/signup/page.tsx
@@ -2,8 +2,37 @@ import styles from '../page.module.scss'
 import Image from "next/image";
 import Link from "next/link";
 import logoImg from '../../../public/logo.svg'
+import { api } from '@/services/api'
+import { redirect } from 'next/navigation';
 
 export default function Signup() {
+
+    async function handleRegister(formData: FormData) {
+        "use server"
+
+        const name = formData.get('name')
+        const email = formData.get('email')
+        const password = formData.get('password')
+
+        if( name === '' || email === '' || password === '' ) {
+            console.log('PREENCHA TODOS OS CAMPOS')
+            return
+        }
+
+        try {
+            await api.post('/users', {
+                name,
+                email,
+                password
+            })
+        } catch(err) {
+            console.log('erro')
+            console.log(err)
+        }
+
+        redirect('/')
+    }
+
     return (
         <>
             <div className={styles.containerCenter}>
@@ -14,7 +43,7 @@ export default function Signup() {
 
                 <section className={styles.login}>
                     <h1>Criando a sua conta</h1>
-                    <form>
+                    <form action={handleRegister}>
                         <input
                             type='text'
                             required
