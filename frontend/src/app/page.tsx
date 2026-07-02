9bfd9a4d063b7adeed8507f3026086e632732c8b
Arthur Ribeiro Marinho <ribeiromarinhoarthur@gmail.com>
2026-07-01 21:37:42 -0300

    190 - logando o usuário

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,8 +2,39 @@ import styles from './page.module.scss'
 import logoImg from '/public/logo.svg'
 import Image from 'next/image'
 import Link from 'next/link'
+import { api } from '@/services/api'
+import { redirect } from 'next/navigation'
 
 export default function Page() {
+  async function handleLogin(formData: FormData) {
+    "use server"
+
+    const email = formData.get('email')
+    const password = formData.get('password')
+
+    if (email === '' || password === '') {
+      return
+    }
+
+    try {
+      const response = await api.post('/session', {
+        email,
+        password
+      })
+
+      if(!response.data.token) {
+        return
+      }
+
+      console.log(response.data)
+
+    } catch(err) {
+      console.log('err')
+      return
+    }
+
+    redirect('/dashboard')
+  }
 
   return(
     <>
@@ -14,7 +45,7 @@ export default function Page() {
         />
 
         <section className={styles.login}>
-          <form>
+          <form action={handleLogin}>
             <input
               type='email'
               required
