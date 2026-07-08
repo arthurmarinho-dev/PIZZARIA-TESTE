4d422dd0b2b27d93a2d9927369da712df4fcecce
Arthur Ribeiro Marinho <ribeiromarinhoarthur@gmail.com>
2026-07-08 20:19:07 -0300

    396 - estilização do cadastro de produtos

--- frontend/src/app/dashboard/product/page.tsx
+++ frontend/src/app/dashboard/product/page.tsx
@@ -1,8 +1,7 @@
+import { Form } from "./components/form";
 
 export default function Product() {
     return(
-        <main>
-            <h1>Página Novo Produto</h1>
-        </main>
+        <Form/>
     )
 }
