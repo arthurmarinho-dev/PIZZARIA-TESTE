ad8b08a750bae385828162b12e7dba04cd1234cf
Arthur Ribeiro Marinho <ribeiromarinhoarthur@gmail.com>
2026-07-08 22:17:24 -0300

    495 - criando alertas no canto da tela

--- frontend/src/app/dashboard/components/header/index.tsx
+++ frontend/src/app/dashboard/components/header/index.tsx
@@ -7,13 +7,14 @@ import logoImg from '/public/logo.svg'
 import { LogOutIcon } from 'lucide-react'
 import { deleteCookie } from 'cookies-next'
 import { useRouter } from 'next/navigation'
+import { toast } from 'sonner'
 
 export function Header() {
     const router = useRouter()
 
     async function handleLogout() {
         deleteCookie("session", { path: "/" })
-
+        toast.success("Logout Realizado com Sucesso")
         router.replace("/")
     }
 
--- frontend/src/app/dashboard/product/components/form/index.tsx
+++ frontend/src/app/dashboard/product/components/form/index.tsx
@@ -7,6 +7,8 @@ import Image from "next/image"
 import { Button } from "@/app/dashboard/components/button"
 import { api } from "@/services/api"
 import { getCookieClient } from "@/lib/cookieClient"
+import { toast } from "sonner"
+import { useRouter } from "next/navigation"
 
 interface CategoryProps {
     id: string;
@@ -18,6 +20,7 @@ interface Props {
 }
 
 export function Form( {categories}: Props ) {
+    const router = useRouter()
     const [image, setImage] = useState<File>()
     const [previewImage, setPreviewImage] = useState("")
 
@@ -30,6 +33,7 @@ export function Form( {categories}: Props ) {
 
 
         if (!name || !categoryIndex || !price || !description || !image) {
+            toast.warning("Preencha todos os campos")
             return
         }
 
@@ -50,10 +54,12 @@ export function Form( {categories}: Props ) {
         })
         .catch((err) => {
             console.log(err)
+            toast.warning("Falha ao Cadastrar este Produto")
             return
         })
 
-        console.log("CADASTRADO COM SUCESSO!")
+        toast.success("Produto Registrado com Sucesso")
+        router.push("/dashboard")
     }
 
     function handleFile(e: ChangeEvent<HTMLInputElement>) {
@@ -61,7 +67,7 @@ export function Form( {categories}: Props ) {
             const image = e.target.files[0]
 
             if (image.type !== "image/jpeg" && image.type !== "image/png") {
-                console.log("FORMATO PROIBIDO!!!!")
+                toast.warning("Formato não permitido!")
                 return
             }
 
